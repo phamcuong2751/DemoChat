8b2a20314034e5c3f697519134036d67c325a63d
Phạm Phong Phú Cường <phamcuong2751@gmail.com>
2023-07-20 22:21:55 +0700

    Add nes unit test

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("junit:junit:4.13.1")
+
+    // https://mvnrepository.com/artifact/org.mockito/mockito-all
+    testImplementation("org.mockito:mockito-all:1.10.19")
+
 }
 
 tasks.test {
